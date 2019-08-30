6160f4b2e75fa5067806673bfc29af7fb976cc10
Ceryl Bruitzman <Ceryl.Bruitzman@kadaster.nl>
2019-08-30 14:29:06 +0200

    Intial commit

---
+++ src/pages/Cart.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>{{msg}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cart',
+  data() {
+    return {
+      msg: 'Welcome to the Cart Page',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/admin/New.vue
@@ -0,0 +1,37 @@
+<template>
+  <product-form
+    :manufacturers="manufacturers"
+    :model="model"
+    @save-product="addProduct">
+  </product-form>
+</template>
+
+<script>
+import ProductFrom from '@/components/product/ProductForm';
+
+export default {
+  data() {
+    return {
+      model: {},
+      manufacturers: [
+        {
+          _id: 'sam',
+          name: 'Samsung',
+        },
+        {
+          _id: 'apple',
+          name: 'Apple',
+        },
+      ],
+    };
+  },
+  methods: {
+    addProduct(model) {
+      console.log('model', model);
+    },
+  },
+  components: {
+    'product-form': ProductFrom,
+  },
+};
+</script>
